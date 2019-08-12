37c9c06f09f606823a9a1a18746ffa70dedeb9a6
Luis Almeida <info@luis-almeida.com>
2019-08-12 19:51:22 +0100

    Note Model and route file added.

--- server.js
+++ server.js
@@ -10,6 +10,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type -- application/json
 app.use(bodyParser.json());
 
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log("Could not connect to the database. Exiting now...", err);
+    process.exit();
+});
+
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to Easynotes application. Take notes quickly."});
@@ -19,3 +37,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
+
