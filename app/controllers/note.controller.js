bbe1009aebf08601c6da120de1534c6f4619d063
Luis Almeida <info@luis-almeida.com>
2019-08-13 19:41:31 +0100

    Create, findAll and findOne Note functions

---
+++ app/controllers/note.controller.js
@@ -0,0 +1,71 @@
+const Note = require('../models/note.model.js');
+
+// create and save a new note
+exports.create = (req, res) => {
+    // validate the request
+    if (!req.body.content) {
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Create a note
+    const note = new Note({
+        title: req.body.title || "Untitled Note",
+        content: req.body.content
+    });
+
+    // Save Note in the database
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occured while creating the Note"
+        });
+    });
+};
+
+// retrieve and return all notes from the database
+exports.findAll = (req, res) => {
+    Note.find()
+    .then(notes => {
+        res.send(notes);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occured while retrieving notes."
+        });
+    });
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+    Note.findById(req.params.noteId)
+    .then(note => {
+        if (!note) {
+            return res.status(404).send({
+                message: "Note not found with id: " + req.params.noteId
+            });
+        }
+        res.send(note);
+    }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        return res.status(500).send({
+            message: "Error retrieving note with id " + req.params.noteId
+        });
+    });
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {
+
+};
